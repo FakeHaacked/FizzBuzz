c339d3a6fd158b7450377f6a35e1dcaa3d723126
Haacked <haacked@gmail.com>
2013-04-08 16:29:56 -0700

    An ignorant attempt to solve FizzBuzz

---
+++ FizzBuzz/FizzBuzzinator.cs
@@ -0,0 +1,21 @@
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+
+namespace FizzBuzz
+{
+    public class FizzBuzzinator
+    {
+        public IEnumerable<string> Generate()
+        {
+            for (int i = 1; i <= 100; i++)
+            {
+                if (i % 15 == 0) yield return "FizzBuzz";
+                if (i % 3 == 0) yield return "Fizz";
+                if (i % 5 == 0) yield return "Buzz";
+                yield return i.ToString(CultureInfo.InvariantCulture);
+            }
+        }
+    }
+}
+
