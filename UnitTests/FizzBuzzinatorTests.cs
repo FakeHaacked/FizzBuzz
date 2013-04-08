5b83bb7bf99a6dbbdd7241822ff46c79fa57a6d8
Haacked <haacked@gmail.com>
2013-04-08 16:58:17 -0700

    Add unit test

---
+++ UnitTests/FizzBuzzinatorTests.cs
@@ -0,0 +1,24 @@
+﻿using System.Linq;
+using FizzBuzz;
+using Xunit;
+
+namespace UnitTests
+{
+    public class FizzBuzzinatorTests
+    {
+        public class TheGenerateMethod
+        {
+            [Fact]
+            public void GeneratesTheRightValues()
+            {
+                var fizzBuzz = new FizzBuzzinator();
+
+                string[] values = fizzBuzz.Generate().Take(3).ToArray();
+
+                Assert.Equal("1", values[0]);
+                Assert.Equal("2", values[1]);
+                Assert.Equal("Fizz", values[2]);
+            }
+        }
+    }
+}
