ad78c255169dd10e50c49f21bf9df47c19c7cd8f
Haacked <haacked@gmail.com>
2013-04-08 16:29:56 -0700

    An ignorant attempt to solve FizzBuzz

---
+++ FizzBuzz/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Linq;
+
+namespace FizzBuzz
+{
+    class Program
+    {
+        const int columnCount = 5;
+        const int totalCount = 100;
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Press any key to get your FizzBuzz on!");
+            Console.ReadKey();
+
+            var fizzBuzzinator = new FizzBuzzinator();
+            fizzBuzzinator.Generate()
+                          .Take(totalCount)
+                          .Select((entry, index) => new {entry, number = index + 1})
+                          .GroupBy(x => x.number % (totalCount/columnCount))
+                          .Select(x => String.Join("  ", x.Select(y => Pad(y.entry)).ToArray()))
+                          .ToList()
+                          .ForEach(Console.WriteLine);
+
+            Console.WriteLine("Press any key to quit.");
+            Console.ReadKey();
+            Console.WriteLine("QUITTER!");
+        }
+
+        private static string Pad(string value)
+        {
+            return value.PadLeft("FizzBuzz".Length, ' ');
+        }
+    }
+}
